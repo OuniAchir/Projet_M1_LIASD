c6c01ef611a7ae717d2bafdd0f8a575196be2ec2
OuniAchir <153436709+OuniAchir@users.noreply.github.com>
2024-08-05 19:26:21 +0200

    Inférence.py

--- Inférence.py
+++ Inférence.py
@@ -62,4 +62,44 @@ def setup_model_and_tokenizer(base_model):
         f"trainable params: {trainable_params} || all params: {all_param} || trainables%: {100 * trainable_params / all_param}"
     )
 
+def generate_response(model, tokenizer, question):
+  # Créer un pipeline de génération de texte avec des paramètres ajustés
+  pipe_inf = pipeline(
+      task="text-generation",
+      model=model,
+      tokenizer=tokenizer,
+      num_return_sequences=1,
+      max_length=500,
+      do_sample=True,
+      temperature=0.7,
+  )
 
+  # Générer la réponse
+  response = pipe_inf(question)
+  
+  # Retourner la réponse
+  return response[0]['generated_text']
+
+def main():
+  
+    # Configuration du logging
+    logging.basicConfig(level=logging.CRITICAL)
+
+    # Définir le modèle de base
+    base_model = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+
+    # Configurer le modèle et le tokenizer
+    model, tokenizer = setup_model_and_tokenizer(base_model)
+
+    # Imprimer les paramètres entraînables
+    print_trainable_parameters(model)
+
+    # Question complexe pour tester la génération de réponse
+    question = "How do the various treatment options for fibromyalgia, to provide a comprehensive management plan for patients, and what role do self-management techniques play in improving patient outcomes?"
+
+    # Générer et afficher la réponse
+    response = generate_response(model, tokenizer, question)
+    print(response)
+
+if __name__ == "__main__":
+    main()
