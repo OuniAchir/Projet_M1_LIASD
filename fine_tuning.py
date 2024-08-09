870ec3e6647409e461427792267bca615d21ffb7
OuniAchir <153436709+OuniAchir@users.noreply.github.com>
2024-08-09 13:12:33 +0200

    Update fine_tuning.py

--- fine_tuning.py
+++ fine_tuning.py
@@ -8,53 +8,7 @@ from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from datasets import load_dataset
 import torch
 from google.colab import drive
-
-def setup_model_and_tokenizer(base_model):
-    # Configuration BitsAndBytes
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
-    )
-
-    # Charger le modèle
-    model = AutoModelForCausalLM.from_pretrained(
-        base_model,
-        device_map="auto",
-        trust_remote_code=True,
-        quantization_config=bnb_config,
-    )
-
-    # Activer gradient_checkpointing
-    model.gradient_checkpointing_enable()
-
-    # Préparer le modèle pour k-bit training
-    model = prepare_model_for_kbit_training(model)
-
-    # Configuration LoRA
-    config = LoraConfig(
-        r=16,
-        lora_alpha=32,
-        target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj"],
-        lora_dropout=0.05,
-        bias="none",
-        task_type="CAUSAL_LM",
-    )
-
-    # Appliquer LoRA au modèle
-    model = get_peft_model(model, config)
-    model.config.use_cache = False
-    model.config.pretraining = 1
-
-    # Charger le tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
-    tokenizer.padding_side = 'right'
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.add_eos_token = True
-    tokenizer.bos_token = tokenizer.eos_token
-
-    return model, tokenizer
+from setup_model import setup_model_and_tokenizer
 
 def train_model(model, tokenizer, dataset):
     # Hyperparameter
