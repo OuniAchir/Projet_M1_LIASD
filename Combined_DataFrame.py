7730c39aca2ee40ce7d03cd27cc9507fc68adc88
OuniAchir <153436709+OuniAchir@users.noreply.github.com>
2024-08-27 18:16:18 +0200

    Update Combined_DataFrame.py

--- Combined_DataFrame.py
+++ Combined_DataFrame.py
@@ -2,23 +2,23 @@ import pandas as pd
 import pyarrow as pa
 from datasets import Dataset
 from scrapping_PMC import main as load_pmc
-from Scrapping_ArXiv import main as load_arxiv
+#from Scrapping_ArXiv import main as load_arxiv
 from Load_Data import main as load_data
 
 def load_pmc_data():
     return load_pmc()
 
-def load_arxiv_data():
-    return load_arxiv()
+#def load_arxiv_data():
+    #return load_arxiv()
 
 def load__data():
     return load_data()
 
 def combine_all_data():
     df_pmc = load_pmc_data()
-    df_arxiv = load_arxiv_data()
+    #df_arxiv = load_arxiv_data()
     df_load = load__data()
-    combined_df = pd.concat([df_pmc, df_arxiv, df_load], ignore_index=True)
+    combined_df = pd.concat([df_pmc, df_load], ignore_index=True)
     return combined_df
 
 def save_to_csv(df, filename='combined_data.csv'):
